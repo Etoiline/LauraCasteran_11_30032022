4bdf396d6fbfedbaf33d7495e37b47ddb724cd58
Laura Casteran <laurac644@gmail.com>
2022-04-16 19:36:46 +0200

    Correction warning createRoot

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './styles/desktop/index.css'
 import './styles/mobile/index.mobi.css'
@@ -11,7 +11,8 @@ import Logement from './pages/logement/Logement'
 
 
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Router>
       <Routes>
@@ -24,5 +25,4 @@ ReactDOM.render(
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
 )
