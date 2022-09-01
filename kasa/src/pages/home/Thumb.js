cada66b3f7ee4b925df1fc21dc12d2266bdff31d
Laura Casteran <laurac644@gmail.com>
2022-09-01 11:39:02 +0200

    install react gh pages

--- kasa/src/pages/home/Thumb.js
+++ kasa/src/pages/home/Thumb.js
@@ -3,6 +3,8 @@ import '../../styles/mobile/Thumb.mobi.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+/* Thumb correspond à la miniature de chaque logement affiché sur la page d'accueil */
+
 function Thumb ({id,cover, name}) {
   const urlLogement = '/logement/'+id
   return (
