fa30aff637fa8af3be7c2acd7c188ace93b1e12c
Laura Casteran <laurac644@gmail.com>
2022-09-01 11:38:44 +0200

    Updates

---
+++ precache-manifest.92a9e57342700486e0bc0aca7cba0503.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0eda9b9a1f7b3d942621",
+    "url": "/LauraCasteran_11_30032022/static/css/main.11873ec1.chunk.css"
+  },
+  {
+    "revision": "0eda9b9a1f7b3d942621",
+    "url": "/LauraCasteran_11_30032022/static/js/main.b86d8427.chunk.js"
+  },
+  {
+    "revision": "b702830eba5a967e0656",
+    "url": "/LauraCasteran_11_30032022/static/js/runtime~main.60fd1bb7.js"
+  },
+  {
+    "revision": "42979a76bab2a359a730",
+    "url": "/LauraCasteran_11_30032022/static/js/2.f812a73e.chunk.js"
+  },
+  {
+    "revision": "389a8da7c6d30647ce97c73a0b758a72",
+    "url": "/LauraCasteran_11_30032022/static/media/Background.389a8da7.png"
+  },
+  {
+    "revision": "a4b81916d24fd7cf12ecd6788c37a58c",
+    "url": "/LauraCasteran_11_30032022/static/media/aPropos.a4b81916.png"
+  },
+  {
+    "revision": "7d799f28d01cfe103b67f526006600df",
+    "url": "/LauraCasteran_11_30032022/static/media/aPropos.mobi.7d799f28.png"
+  },
+  {
+    "revision": "a124d9d8c7743e8095bf0bb74f0a25a7",
+    "url": "/LauraCasteran_11_30032022/index.html"
+  }
+];
